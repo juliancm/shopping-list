62da088fdedac39042d1d1c43bdf43d139c9ef79
juliancm <56898577+juliancm@users.noreply.github.com>
2019-11-13 18:57:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+$("#shoppingForm").on('submit', function(e){
+	var text = $('#addToList').val();
+  $('#shoppingList').append($('<li>').append(text));
+  $('#addToList').val('');
+  e.preventDefault();
+});
 
+$('.shopping-list').on('click', 'shopping-item-delete', function(){
+	$(this).remove();
+});
+
+
+
+
+/*
+
+The hash (#) specifies to select elements by their ID's
+
+The dot (.) specifies to select elements by their classname
+
+*/
